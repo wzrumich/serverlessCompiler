bc4e25d18290a03f91470832ac649e2edfb3a873
Ishaan Pota <ishaan.pota@gmail.com>
2021-04-17 10:27:24 -0700

    Code generation

---
+++ stackTraceAnalysis/inlinedCode.py
@@ -0,0 +1,15 @@
+
+import sys
+import traceback
+_sys.path.insert(1, '/Users/ishaanpota/Desktop/serverlessCompiler/stackTraceAnalysis')
+import stackTraceAnalysis
+
+def main():
+    print('wtaf')
+    call1_z = 4
+    print('here')
+    call4_y = 9
+    call4_x = 0
+    virtual_return_register_0 = call4_x
+    virtual_return_register_0
+    f = call1_z
--- stackTraceAnalysis/stackTraceAnalysis.py
+++ stackTraceAnalysis/stackTraceAnalysis.py
@@ -1,3 +1,20 @@
+import sys
+sys.path.insert(1, '/Users/ishaanpota/Desktop/serverlessCompiler/inliner')
+
+from symbolHelper import *
+from visitor import *
+import copy
+import pprint
+import astunparse
+import astor
+import utils
+import astpretty
+import ast
+import traceback
+import subprocess
+import json
+
+
 def formatMethod(method):
     method = method.split()
 
@@ -14,6 +31,51 @@ def formatMethod(method):
     return finString
 
 
+
+
+def variablesReadInCodeBlock(codeblock1):
+    #Checking RAW dependencies 
+    res1 = set()
+    for anode in ast.walk(ast.parse(codeblock1)):
+    
+        
+        if type(anode).__name__ == 'Assign':
+            if type(anode.value).__name__ == 'Name':
+                print(anode.value.id)
+                res1.add(anode.value.id)
+                
+            
+            
+        elif type(anode).__name__ == 'For':
+            if type(anode.target).__name__ == 'Name':
+                res1.add(anode.target.id)
+
+    #print(res1)
+    
+    return res1
+
+#Returns a list of variables written to code block
+def variablesWrittenInCodeBlock(codeblock1):
+
+    #Checking RAW dependencies 
+    res1 = set()
+    for anode in ast.walk(ast.parse(codeblock1)):
+        
+        if type(anode).__name__ == 'Assign':
+            
+            res1.update(t.id for t in anode.targets if type(t).__name__ == 'Name')
+        elif type(anode).__name__ == 'For':
+            if type(anode.target).__name__ == 'Name':
+                res1.add(anode.target.id)
+    
+    
+    return res1
+
+    
+
+
+
+
 # Objective: Given a list of stack traces corresponding to different phases in the program, map these stack traces to regions in the code and generate separate code for those blocks
 # Then, analayze the dependencies between them and replace those with RDMA calls
 
@@ -26,10 +88,10 @@ def stackTraceCodeMapper(stacktrace, filename):
     fp = open(filename)
     lines = fp.readlines()
 
-    f = open("stacktraceCode.py", "w")
-
     # split stack trace into individual lines
     tokenizedTrace = (stacktrace.split("File"))
+
+    print(stacktrace)
 
     lineNumbersRequired = []
 
@@ -52,11 +114,11 @@ def stackTraceCodeMapper(stacktrace, filename):
             for j in range(0, len(lines)):
 
                 if lines[j].find(last_method_def) != -1:
-                    for l in range(j+1, int(lineNo) + 1):
+                    for l in range(j+2, int(lineNo)):
                         lineNumbersRequired.append(l)
 
         else:
-            # print(tokenizedTrace[i].split()[-1])
+
             lineInd = tokenizedTrace[i].find("line")
             commaInd = tokenizedTrace[i].find(",", lineInd)
             lineNo = tokenizedTrace[i][lineInd + 5:commaInd]
@@ -67,14 +129,171 @@ def stackTraceCodeMapper(stacktrace, filename):
 
             for j in range(0, len(lines)):
                 if lines[j].find(methodDef) != -1:
-                    for l in range(j, int(lineNo) + 1):
+                    print(j)
+                    for l in range(j + 2, int(lineNo)):
                         lineNumbersRequired.append(l)
 
-    f.close()
-
-    return(lineNumbersRequired)
 
     fp.close()
+   
+
+    #Part 2: In Progress: Use the mapping function in the inliner to find the location of the compute block in the inlined code
+
+    newLineNos = []
+    s2_out = subprocess.check_output([sys.executable, "/Users/ishaanpota/Desktop/serverlessCompiler/inliner/inline.py"])
+    inlineOutput = str(s2_out, 'utf-8')
+
+    lineMap = json.loads(inlineOutput.split('\n')[::-1][1])
 
 
-# Part 2: In Progress: Use the mapping function in the inliner to find the location of the compute block in the inlined code
+    #Note that a function can be called multiple times, so the lineMap is going to a map a line in the old code to a list of lines in the new code,
+    #currently treating it like a number
+
+    for i in lineNumbersRequired:
+        if str(i) in lineMap.keys():
+            newLineNos.append(lineMap[str(i)][0])
+
+
+    print(newLineNos)
+    print(lineMap.keys())
+
+
+    #Now, use the newLineNos to separate the code into two compute blocks and do dependency analysis between them, replacing all variable accesses with remote accesses
+    inlinedCode = open("/Users/ishaanpota/Desktop/serverlessCompiler/stackTraceAnalysis/inlinedCode.py","r")
+    computeStackTrace = ""
+    computeNotStackTrace = ""
+
+    computeStackTrace = "def main():" + "\n"
+
+
+    counter = 1 
+
+    for line in inlinedCode.readlines():
+        if counter in newLineNos:
+            computeStackTrace +=  line
+        else:
+            computeNotStackTrace += line 
+        counter +=1
+
+
+    
+    DAG = []
+
+    DAG.append(computeStackTrace)
+    DAG.append(computeNotStackTrace)
+    
+
+    #print(computeStackTrace)
+    #print(computeNotStackTrace)
+
+
+    #Order the compute blocks 
+    #To order the compute blocks, we will just use the line numbers to figure out which block goes before which 
+
+
+    #add code to handle dependent variables across compute blocks
+
+
+    #for all compute blocks:
+
+
+    for i in range(0,len(DAG)):
+    
+        varWrittenList = []
+        varWrittenList = variablesWrittenInCodeBlock(DAG[i])
+        remoteWriteVars = []
+
+        varReadList = []
+        varReadList = variablesReadInCodeBlock(DAG[i])
+        remoteReadVars = []
+
+
+
+        for j in range(i,len(DAG)):
+            varReadList = []
+            varReadList = variablesReadInCodeBlock(DAG[j])
+        
+            for var in varReadList:
+                if var in varWrittenList:
+                    remoteWriteVars.append(var)
+        
+
+        for k in range(i-1,-1,-1):
+            varWriteList = []
+            varWriteList = variablesWrittenInCodeBlock(DAG[k])
+
+            for var in varWriteList:
+                if var in varReadList:
+                    remoteReadVars.append(var)
+        
+        print(remoteReadVars)
+        print(remoteWriteVars)
+        print("========")
+        s = ""
+
+        for i in range(0,len(remoteReadVars)):
+            s = remoteReadVars[i] + " = " + "context_dict[\"" + remoteReadVars[i] + "\"]" + "\n"
+
+
+        codeRead =  ("""action = buffer_pool_lib.action_setup()\ntransport_name = 'client1'\ntrans = action.get_transport(transport_name, 'rdma')\ntrans.reg(buffer_pool_lib.buffer_size)\nbuffer_pool = buffer_pool_lib.buffer_pool(trans, context_dict["buffer_pool_metadata"])""" + "\n" + s)
+
+
+
+        r = ""
+
+
+        for i in range(0,len(remoteWriteVars)):
+            r = "    " + "context_dict[\"" + remoteWriteVars[i] + "\"]" + "=" + remoteWriteVars[i] + "\n"
+
+        #print(r)
+
+
+        
+
+
+        codeWrite =  r + """    context_dict["buffer_pool_metadata"] = buffer_pool.get_buffer_metadata()\n    buffer_pool_lib.write_params(context_dict)"""
+
+
+        c =  codeRead + DAG[i] + codeWrite
+        print(c)
+
+
+
+
+
+
+
+
+            
+        
+
+
+
+            #Insert code to write the written variables to remote memory (Zerui's library calls)
+           
+
+
+
+
+
+
+    
+
+    
+
+
+
+    
+
+
+
+
+
+
+    
+
+    
+
+   
+
+
